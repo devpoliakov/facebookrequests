15c30f035784768826100b06fbb493affcfe07c9
Senquiem <kontynent@ua.fm>
2018-08-02 16:14:11 +0300

    simple user events for status post in the groups

--- putt_status_group.js
+++ putt_status_group.js
@@ -1,15 +1,27 @@
-//https://www.facebook.com/groups/your_group_id/
-    var textareaCont = document.querySelector('textarea[name="xhpc_message_text"]');
-    var formStatusSendus = document.querySelector('form[rel="async"]');
+// https://www.facebook.com/groups/your_group_id/
+// url for sending data
+// https://www.facebook.com/ajax/groups/members/add_post.php
+
+var textareaCont = document.querySelector('textarea[name="xhpc_message_text"]');
+var formTrigger = document.querySelector('div[data-testid="react-composer-root"]');
+var buttonStatusSendus = document.querySelector('button[data-testid="react-composer-post-button"]');
     if (textareaCont)
     {
-        textareaCont.value = "hello world )";
-        if(textareaCont){
-                formStatusSendus.submit();
-            }
-                else{
-                console.log('form wrong...');
-                }
-    }else{
-    console.log('something wrong...');
-    }
+        textareaCont.value = "не балуйте штучний інтелект своїми лайками )";
+        }else{
+        console.log('textareaCont wrong...');
+        }
+
+if(formTrigger){
+        formTrigger.click();
+        console.log('good connection formTrigger');
+        }else{
+        console.log('formTrigger wrong...');
+        }
+
+if(buttonStatusSendus){
+        buttonStatusSendus.click();
+        console.log('good connection buttonStatusSendus');
+        }else{
+        console.log('buttonStatusSendus wrong...');
+        }
